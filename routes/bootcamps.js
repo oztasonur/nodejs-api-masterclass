f33921ac323dda94a6dfb42b9f13f41a7f97dd7d
Onur Öztaş <github@onuroztas.com>
2023-07-11 10:50:42 +0300

    Create bootcamp controllers
    Refactor route implementation to utilize bootcamp controllers for handling all requests. This commit ensures that bootcamp functionality is separated and centralized within dedicated controller files, which are then invoked from the routes.

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,28 +1,19 @@
 const express = require("express");
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ succes: true, msg: "Show all bootcamps" });
-});
+router.route("/").get(getBootcamps).post(createBootcamp);
 
-router.get("/:id", (req, res) => {
-  res.status(200).json({ succes: true, msg: `Get bootcamp ${req.params.id}` });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({ succes: true, msg: "Create new bootcamp" });
-});
-
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ succes: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ succes: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
