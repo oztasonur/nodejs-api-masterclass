9dd836fa817cf933ba2a2b6ce41e75e5bd50387a
Onur Öztaş <github@onuroztas.com>
2023-08-08 13:24:36 +0300

    Use bootcamp model in createBootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const Bootcamp = require("../models/Bootcamp");
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
@@ -15,8 +16,18 @@ exports.getBootcamp = (req, res, next) => {
 // @desc        Create new bootcamp
 // @route       POST /api/v1/bootcamps
 // @access      Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ succes: true, msg: "Create new bootcamp" });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 // @desc        Update bootcamp
