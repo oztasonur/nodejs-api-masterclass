122116b335edab5a0ff3c678bce17ae1592a60dc
Onur Öztaş <github@onuroztas.com>
2023-08-09 14:13:06 +0300

    Add advanced filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,11 +2,23 @@ const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const geocoder = require("../utils/geocoder");
 const Bootcamp = require("../models/Bootcamp");
+
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.find();
+  let query;
+
+  let queryStr = JSON.stringify(req.query);
+
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  const bootcamp = await query;
   res
     .status(200)
     .json({ success: true, count: bootcamp.length, data: bootcamp });
